666c275f7fec020b550912d0dbd329aea2443122
Vernon G <vern_go_123@yahoo.com>
2011-03-15 20:22:41 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+
+	<head>
+		<title></title>
+		
+		<?
+		
+		?>
+		
+	</head>
+
+	<body>
+		<form name = "userview" action = "" method = "POST">
+			<p>Which type of user are you?</p>
+			<input type = "radio" name = "group1" value = "admin" checked = "true"/>Administrator<br>
+			<input type = "radio" name = "group1" value = "man"/>Manager<br>
+			<input type = "radio" name = "group1" value = "seller"/>Sales Agent<br>
+			<input type = "submit" value = "Log In" />
+		</form>
+	</body>	
+
+</html>

550b5ab9fe22b1f6136e60d0b15c4b93f1febf74
Vernon G <vern_go_123@yahoo.com>
2011-03-16 19:05:21 +0800

    Implemented Warehouse Delivery Forms.

---
+++ dynamictable.jsm
@@ -0,0 +1,57 @@
+var EXPORTED_SYMBOLS = ["hideTemplateRows", "addRow", "deleteRow"];
+
+function hideTemplateRows(tableID)
+{
+	for(var i = 0; i < tableID.length; i++)
+	{
+		var table = document.getElementById(tableID[i]);
+		table.rows[1].style.visibility = 'hidden';
+		table.rows[1].style.display = 'none';
+	}
+}
+
+function addRow(tableID) 
+{
+	var table = document.getElementById(tableID);
+	var rowCount = table.rows.length;
+	var row = table.insertRow(rowCount);
+	var colCount = table.rows[1].cells.length;
+	
+	for(var i = 0; i < colCount; i++) 
+	{
+		var newcell = row.insertCell(i);
+		newcell.innerHTML = table.rows[1].cells[i].innerHTML;
+		
+		switch(newcell.childNodes[0].type) 
+		{
+			case "text":
+				newcell.childNodes[0].value = "";
+				break;
+			case "checkbox":
+				newcell.childNodes[0].checked = false;
+				break;
+			case "select-one":
+				newcell.childNodes[0].selectedIndex = 0;
+				break;
+		}
+		
+	}
+}
+
+function deleteRow(tableID) 
+{
+	var table = document.getElementById(tableID);
+	var rowCount = table.rows.length;
+
+	for(var i = 2; i < rowCount; i++) 
+	{
+		var row = table.rows[i];
+		var checkbox = row.cells[0].childNodes[0];
+		if(checkbox != null && checkbox.checked) 
+		{
+			table.deleteRow(i);
+			rowCount--;
+			i--;
+		}
+	}
+}
--- warehouse.php
+++ warehouse.php
@@ -1,5 +1,227 @@
 <html>
-<body>
-<p>Warehouse Staff</p>
-</body>
+
+	<head>
+		<title></title>
+		<script type = "text/javascript" src = "dynamictable.jsm"></script>
+		<script type = "text/javascript">
+			
+			function initialize()
+			{
+				updateDateTime();
+				setInterval(updateDateTime, 1000);
+				updateYearChoice();
+				addMonthChoice();
+				updateDayChoice();
+				addHourChoice();
+				addMinuteChoice();
+				var tableArray = new Array("deliveryTable");
+				hideTemplateRows(tableArray);
+			}
+			
+			function updateDateTime()
+			{
+				if(document.getElementById("manualdt").checked)
+				{
+					document.getElementById("datetime").style.visibility = 'visible';
+					document.getElementById("datetime").style.display = 'block';
+				}
+				else
+				{
+					document.getElementById("datetime").style.visibility = 'hidden';
+					document.getElementById("datetime").style.display = 'none';
+					updateYearChoice();
+					document.getElementById("yearchoice").selectedIndex = 0;
+					document.getElementById("monthchoice").selectedIndex = (new Date()).getMonth();
+					updateDayChoice();
+					document.getElementById("daychoice").selectedIndex = (new Date()).getDate() - 1;
+					document.getElementById("hourchoice").selectedIndex = (new Date()).getHours();
+					document.getElementById("minutechoice").selectedIndex = (new Date()).getMinutes();
+				}
+			}
+			
+			function updateYearChoice()
+			{
+				for(var i = document.getElementById("yearchoice").options.length - 1; i >= 0; i--)
+					document.getElementById("yearchoice").remove(i);
+					
+				var i = (new Date()).getFullYear();
+				for (var c = 0; c < 25; c++)
+				{
+					var option = document.createElement("OPTION");
+					option.text = i;
+					option.value = i;
+					document.getElementById("yearchoice").options.add(option);
+					i--;
+				}
+			}
+			
+			function addMonthChoice()
+			{
+				var months = new Array();
+				months[1] =	"January";
+				months[2] =	"February";
+				months[3] =	"March";
+				months[4] =	"April";
+				months[5] =	"May";
+				months[6] =	"June";
+				months[7] =	"July";
+				months[8] =	"August";
+				months[9] =	"September";
+				months[10] = "October";
+				months[11] = "November";
+				months[12] = "December";
+				
+				for (var i = 1; i < months.length; i++)
+				{
+					var option = document.createElement("OPTION");
+					option.text = months[i];
+					if((i + "").length == 1)
+						option.value = "0" + i;
+					else if((i + "").length == 2)
+						option.value = i;
+					document.getElementById("monthchoice").options.add(option);
+				}
+			}
+			
+			function updateDayChoice()
+			{
+				for(var i = document.getElementById("daychoice").options.length - 1; i >= 0; i--)
+					document.getElementById("daychoice").remove(i);
+					
+				for (var i = 1; i <= 28; i++)
+				{
+					var option = document.createElement("OPTION");
+					option.text = i;
+					if((i + "").length == 1)
+						option.value = "0" + i;
+					else if((i + "").length == 2)
+						option.value = i;
+					document.getElementById("daychoice").options.add(option);
+				}
+				
+				var month = document.getElementById("monthchoice").selectedIndex + 1;
+				var yearchoice = document.getElementById("yearchoice");
+				var year = yearchoice.options[yearchoice.selectedIndex].value;
+
+				if((month == 2 && year % 4 == 0) || month != 2)
+				{
+					var option29 = document.createElement("OPTION");
+					option29.text = 29;
+					option29.value = 29;
+					document.getElementById("daychoice").options.add(option29);
+				}
+				if(month != 2)
+				{
+					var option30 = document.createElement("OPTION");
+					option30.text = 30;
+					option30.value = 30;
+					document.getElementById("daychoice").options.add(option30);
+					if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+					{
+						var option31 = document.createElement("OPTION");
+						option31.text = 31;
+						option31.value = 31;
+						document.getElementById("daychoice").options.add(option31);
+					}
+				}
+			}
+			
+			function addHourChoice()
+			{
+				for (var i = 0; i < 24; i++)
+				{
+					var option = document.createElement("OPTION");
+					if((i + "").length == 1)
+					{
+						option.text = "0" + i;
+						option.value = "0" + i;
+					}
+					else if((i + "").length == 2)
+					{
+						option.text = i;
+						option.value = i;
+					}
+					document.getElementById("hourchoice").options.add(option);
+				}
+			}
+			
+			function addMinuteChoice()
+			{
+				for (var i = 0; i < 60; i++)
+				{
+					var option = document.createElement("OPTION");
+					if((i + "").length == 1)
+					{
+						option.text = "0" + i;
+						option.value = "0" + i;
+					}
+					else if((i + "").length == 2)
+					{
+						option.text = i;
+						option.value = i;
+					}
+					document.getElementById("minutechoice").options.add(option);
+				}
+			}
+				
+		</script>
+	</head>
+
+	<body onload = "initialize()">
+		<h1>Warehouse Staff</h1>
+		<h2>What would you like to do?</h2>
+		
+		<p><input type = "checkbox" id = "manualdt" name = "manualdt" onclick = "updateDateTime()"> 
+			Tick to manually set the date and time.
+		</p>
+		<div id = "datetime">		
+			<p>Date: 
+				<select id = "yearchoice" onchange = "updateDayChoice()" name = "yearchoice"></select>
+				<select id = "monthchoice" onchange = "updateDayChoice()" name = "monthchoice"></select>
+				<select id = "daychoice" name = "daychoice"></select>
+			</p>
+				
+			<p>Time: 
+				<select id = "hourchoice" name = "hourchoice"></select> 
+				<select id = "minutechoice" name = "minutechoice"></select>
+			</p>
+		</div>
+			
+		<h3>Accept a New Delivery</h3>
+		<form name = "deli" action = "redirect.php" method = "post">
+			<p>Delivered by: <input type = "text" name = "supplier" value = "Supplier's Name"></p>
+			<p>Received by: <input type = "text" name = "supplier" value = "Staff's Name"></p>
+			
+			<caption>Delivery Items</caption>
+			<table id = "deliveryTable">
+				<th>
+					<td>Item Type</td>
+					<td>Cost</td>
+					<td>Quantity</td>
+				</th>
+				<tr>
+					<td><input type = "checkbox" name = "checkbox"/></td>
+					<td><select name= "itemType"></select></td>
+					<td><input type = "text" name = "cost"/></td>
+					<td><input type = "text" name = "quantity"/></td>
+				</tr>
+			</table>
+			
+			<input type = "button" value = "Add New Delivery Item" onclick = "addRow('deliveryTable')"/>
+			<input type = "button" value = "Delete Selected Delivery Items" onclick = "deleteRow('deliveryTable')"/>
+			<br>
+			<input type = "submit" value = "Accept Delivery">
+		</form>
+		
+		<h3>Issue Item to Sales Agent</h3>
+		<form name = "issue" action = "redirect.php" method = "post">
+			<input type = "submit" value = "Issue Item">
+		</form>
+		
+		
+	
+		<p id = "debug">
+		</p>
+	</body>
+	
 </html>

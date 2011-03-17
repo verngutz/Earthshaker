37002a25f3987a4ae150934e6a8dbac18831c909
Vernon G <vern_go_123@yahoo.com>
2011-03-18 04:59:20 +0800

    deleted old redirect script

--- redirect.php
+++
@@ -1,5 +0,0 @@
-<?
-	$con = mysql_connect("localhost", "root", "root") or die('Could not connect: ' . mysql_error());
-	mysql_select_db("distribution", $con);
-	header("Location: " . $_POST["group1"] . ".php");
-?>

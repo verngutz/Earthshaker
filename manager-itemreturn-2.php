6d736737696ebad634412d89288147c863ed5fca
unknown <Administrator@.(none)>
2011-03-19 02:18:39 +0800

    zzz

--- manager-itemreturn-2.php
+++ manager-itemreturn-2.php
@@ -4,7 +4,9 @@
 		<?php
 		$con = mysql_connect("localhost", "root", "root") or die('Could not connect: ' . mysql_error());
 		mysql_select_db("distribution", $con);
-		$result = mysql_query("SELECT b.batchno as 'batchno', r.returndate as 'returndate', concat(s.agentlastname, ', ', s.agentfirstname) as 'agentname', c.clientname as 'clientname' FROM batch b, salesagent s, client c, itemreturn r WHERE batch.batchno=$_POST['input1'] AND batch.agentid=salesagent.agentid AND salesagent.clientid=client.clientid AND batch.batchno=itemreturn.batchno LIMIT 1");
+		$result = mysql_query("SELECT b.batchno as 'batchno', r.returndate as 'returndate', concat(s.agentlastname, ', ', s.agentfirstname) as 'agentname', c.clientname as 'clientname', i.itemcode as 'itemcode', i.description as 'desc', ib.quantity as 'quant'
+			FROM batch b, salesagent s, client c, itemreturn r, item i, itemxbatch ib
+			WHERE batch.agentid=salesagent.agentid AND salesagent.clientid=client.clientid AND batch.batchno=itemreturn.batchno AND itemxbatch.batchno=batch.batchno AND item.itemcode=itemxbatch.itemcode AND batch.batchno=itemreturn.batchno");
 
 		echo "<table border='1'>
 		<tr>
@@ -12,6 +14,9 @@
 		<th>Date</th>
 		<th>Agent</th>
 		<th>Client</th>
+		<th>Item</th>
+		<th>Description</th>
+		<th>Qty</th>		
 		</tr>";
 
 		while($row = mysql_fetch_array($result))
@@ -21,24 +26,6 @@
 		  echo "<td>" . $row['returndate'] . "</td>";
 		  echo "<td>" . $row['agentname'] . "</td>";
 		  echo "<td>" . $row['clientname'] . "</td>";
-		  echo "</tr>";
-		  }
-		echo "</table>";
-		
-		echo "<br>";
-		
-		$result2 = mysql_query("SELECT i.itemcode as 'itemcode', i.description as 'desc', ib.quantity as 'quant' FROM item i, itemxbatch ib, itemreturn r, batch b WHERE batch.batchno=$_POST['input1'] AND itemxbatch.batchno=batch.batchno AND item.itemcode=itemxbatch.itemcode AND batch.batchno=itemreturn.batchno");
-		
-		echo "<table border='1'>
-		<tr>
-		<th>Item</th>
-		<th>Description</th>
-		<th>Qty</th>
-		</tr>";
-		
-		while($row = mysql_fetch_array($result2))
-		  {
-		  echo "<tr>";
 		  echo "<td>" . $row['itemcode'] . "</td>";
 		  echo "<td>" . $row['desc'] . "</td>";
 		  echo "<td>" . $row['quant'] . "</td>";
@@ -46,6 +33,7 @@
 		  }
 		echo "</table>";
 		
+		echo "<br>";
 		
 		mysql_close($con);
 		?> 

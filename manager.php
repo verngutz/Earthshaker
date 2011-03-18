418911b9731061aa0378b92e69a30af761ad4fc3
unknown <Administrator@.(none)>
2011-03-19 02:40:38 +0800

    zzz

--- manager.php
+++ manager.php
@@ -11,11 +11,11 @@
 	<body>
 		<form name = "userview" action = "redirect.php" method = "POST">
 			<p>What report would you like to generate?</p>
-			<input type = "radio" name = "group1" value = "manager-delivery" checked = "true"/>Delivery Receipt<br>
-			<input type = "radio" name = "group1" value = "manager-invoice"/>Sales Invoice<br>
-			<input type = "radio" name = "group1" value = "manager-itemissuance"/>Item Issuance Form<br>
-			<input type = "radio" name = "group1" value = "manager-itemreturn"/>Item Return Form<br>
-			<input type = "radio" name = "group1" value = "manager-itemtransfer"/>Item Transfer Form<br>
+			<input type = "radio" name = "group1" value = "manager-delivery-2" checked = "true"/>Delivery Receipt<br>
+			<input type = "radio" name = "group1" value = "manager-invoice-2"/>Sales Invoice<br>
+			<input type = "radio" name = "group1" value = "manager-itemissuance-2"/>Item Issuance Form<br>
+			<input type = "radio" name = "group1" value = "manager-itemreturn-2"/>Item Return Form<br>
+			<input type = "radio" name = "group1" value = "manager-itemtransfer-2"/>Item Transfer Form<br>
 			<input type = "submit" value = "Print" />
 		</form>
 	</body>	

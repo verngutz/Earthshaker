b13911a4a55385e41c52f5dcdb57f2ad5eaab640
unknown <Administrator@.(none)>
2011-03-21 00:37:53 +0800

    lala

--- manager-itemissuance-2.php
+++ manager-itemissuance-2.php
@@ -7,11 +7,11 @@
 		
 		$result = mysql_query("
 		
-		SELECT b.batchno as 'batchno', is.issuedate as 'batchdate', concat(s.agentlastname, ', ', s.agentfirstname) as 'agentname',
+		SELECT b.batchno as 'batchno', iss.issuedate as 'batchdate', concat(s.agentlastname, ', ', s.agentfirstname) as 'agentname',
 		c.clientname as 'clientname', concat(st.stafflastname,' ,',st.stafffirstname) as 'issuer', i.itemcode as 'itemcode',
-		i.description as 'desc', is.quantity as 'quant', i.srp as 'srp'
-		FROM batch b, salesagent s, client c, item i, staff st, issuance is
-		WHERE b.batchno=is.batchno AND s.agentid=is.agentid AND c.clientid=s.clientid AND i.itemcode=is.itemcode AND st.staffid=is.staffid
+		i.description as 'desc', iss.quantity as 'quant', i.srp as 'srp'
+		FROM batch b, salesagent s, client c, item i, staff st, issuance iss
+		WHERE b.batchno=iss.batchno AND s.agentid=iss.agentid AND c.clientid=s.clientid AND i.itemcode=iss.itemcode AND st.staffid=iss.staffid
 		ORDER BY b.batchno
 		
 		");
